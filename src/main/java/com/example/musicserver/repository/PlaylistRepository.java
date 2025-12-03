39e231faa590699103df95c34a20a45f08151888
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-12-04 00:40:49 +0200

    Delete src directory

--- src/main/java/com/example/musicserver/repository/PlaylistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.musicserver.repository;
-
-import com.example.musicserver.entity.PlaylistEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaylistRepository extends JpaRepository<PlaylistEntity, Long> {
-}
--- src/main/java/com/example/musicserver/repository/TrackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.musicserver.repository;
-
-import com.example.musicserver.entity.TrackEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrackRepository extends JpaRepository<TrackEntity, Long> {
-}
